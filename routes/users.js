47a03e6979da44d2a38c2a47aae43ebabca37555
Ragnar Öst <ragnar@patronum.se>
2021-11-26 01:07:55 +0100

    updated user count that was not working, document count is outdated and need to be handled like a promise

--- routes/users.js
+++ routes/users.js
@@ -130,15 +130,33 @@ router.delete('/:id', (req, res) => {
     });
 });
 
-router.get(`/get/count`, async (req, res) => {
-  const userCount = await User.countDocuments((count) => count);
-
-  if (!userCount) {
-    res.status(500).json({ success: false });
-  }
-  res.send({
-    userCount: userCount,
-  });
+router.get(`/get/count`, (req, res) => {
+  User.countDocuments()
+    .then((count) => {
+      if (count) {
+        return res.status(200).json({ userCount: count });
+      } else {
+        return res.status(500).json({ success: false });
+      }
+    })
+    .catch((err) => {
+      return res.status(400).json({
+        success: false,
+        error: err,
+      });
+    });
 });
+
+// old version
+// router.get(`/get/count`, async (req, res) => {
+//   const userCount = await User.countDocuments((count) => count);
+
+//   if (!userCount) {
+//     res.status(500).json({ success: false });
+//   }
+//   res.send({
+//     userCount: userCount,
+//   });
+// });
 
 module.exports = router;
